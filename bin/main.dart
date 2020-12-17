7290103f82d5e5d4fc945c1ab4cdeeb98adde492
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2020-12-18 06:50:14 +0700

    List Basic

--- bin/main.dart
+++ bin/main.dart
@@ -1,37 +1,13 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  print(sayNameParameter('taufan', 'oke bro', to: 'azizah', appName: 'wa'));
-  print(sayPositionalParameter('taufan', 'oke bro', 'azizah', 'wa'));
+  List<int> lists = [1, 2, 3, 4, 5];
 
-  Function luas;
-  luas = luas_segiempat;
+  for (int list in lists) {
+    print(list);
+  }
 
-  print(luas(10.0, 5.0));
-
-  print(doMathOperator(1, 2, (a, b) => a + b));
+  lists.forEach((list) {
+    print(list);
+  });
 }
-
-int doMathOperator(int number1, int number2, Function(int, int) operator) {
-  return operator(number1, number2);
-}
-
-String sayNameParameter(String from, String message,
-    {String to, String appName}) {
-  return from +
-      " say " +
-      message +
-      ((to != null) ? " to " + to : "") +
-      ((appName != null) ? " via " + appName : "");
-}
-
-String sayPositionalParameter(String from, String message,
-    [String to, String appName]) {
-  return from +
-      " say " +
-      message +
-      ((to != null) ? " to " + to : "") +
-      ((appName != null) ? " via " + appName : "");
-}
-
-double luas_segiempat(double panjang, double lebar) => panjang * lebar;
