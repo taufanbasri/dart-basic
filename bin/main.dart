69d7f689e436bd83794488a2428a084210534421
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2020-12-17 11:59:57 +0700

    While Loop

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  for (int counter = 0; counter < 5; counter++) {
-    print(counter);
+  int i = 0;
+
+  while (i < 5) {
+    print(i);
+    i++;
   }
 }
