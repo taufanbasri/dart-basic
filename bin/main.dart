beacb539df7fa6c563354da3a8059593991593d6
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2020-12-17 11:24:37 +0700

    For Loop

--- bin/main.dart
+++ bin/main.dart
@@ -1,23 +1,7 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  int number = int.tryParse(stdin.readLineSync());
-  String output;
-
-  switch (number) {
-    case 1:
-      output = 'satu';
-      break;
-    case 2:
-      output = '2';
-      break;
-    case 3:
-      output = 'tiga';
-      break;
-    default:
-      output = 'nol atau negatif';
-      break;
+  for (int counter = 0; counter < 5; counter++) {
+    print(counter);
   }
-
-  print(output);
 }
