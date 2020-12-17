407f6370a64f54a4805527ad44f76a2df0a28c7b
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2020-12-18 06:34:02 +0700

    Lambda Expression

--- bin/main.dart
+++ bin/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 void main(List<String> arguments) {
   print(sayNameParameter('taufan', 'oke bro', to: 'azizah', appName: 'wa'));
   print(sayPositionalParameter('taufan', 'oke bro', 'azizah', 'wa'));
+  print(luas_segiempat(10.0, 5.0));
 }
 
 String sayNameParameter(String from, String message,
@@ -22,3 +23,5 @@ String sayPositionalParameter(String from, String message,
       ((to != null) ? " to " + to : "") +
       ((appName != null) ? " via " + appName : "");
 }
+
+double luas_segiempat(double panjang, double lebar) => panjang * lebar;
