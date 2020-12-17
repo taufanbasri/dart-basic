53d0f5e4c6223b6338e2d809d260bc1a9115b506
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2020-12-17 10:44:31 +0700

    If Else

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,16 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  String input = stdin.readLineSync();
-  int number = int.tryParse(input);
+  int number = int.tryParse(stdin.readLineSync());
+  String output;
 
-  print(number + 10);
+  if (number > 0) {
+    output = 'positif';
+  } else if (number < 0) {
+    output = 'negativ';
+  } else {
+    output = 'bilangan 0 atau karakter';
+  }
+
+  print(output);
 }
