7549540645c32fca23d7fc2afc5cc4d830965f10
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2020-12-19 19:23:47 +0700

    List Operation

--- bin/main.dart
+++ bin/main.dart
@@ -1,13 +1,30 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  List<int> lists = [1, 2, 3, 4, 5];
+  List<int> myList = [1, 2, 3, 4, 5];
+  List<int> lists = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  for (int list in lists) {
-    print(list);
+  myList.add(10);
+  myList.addAll(lists);
+  myList.insert(1, 20);
+  myList.insertAll(2, [30, 40]);
+  myList.remove(5);
+  myList.removeLast();
+  myList.removeAt(6);
+  myList.removeRange(4, 6); // remove at index 4 until 5
+  myList.removeWhere((number) => number % 2 != 0);
+  myList.sort((a, b) => a - b);
+
+  Set<int> s;
+  s = myList.toSet(); // membuat isi list menjadi unik, tidak ada yang sama
+
+  if (myList.every((number) => number % 2 != 0)) {
+    print('semua ganjil');
+  } else {
+    print('tidak semua ganjil');
   }
 
-  lists.forEach((list) {
+  myList.forEach((list) {
     print(list);
   });
 }
