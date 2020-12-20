750ee769af62c1e3297e02a8680a18ce17df871d
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2020-12-20 15:16:37 +0700

    Constructor inheritance with optional parameter

--- bin/person.dart
+++ bin/person.dart
@@ -1,7 +1,7 @@
 class Person {
   String name;
 
-  Person(String name) {
+  Person({String name}) {
     print('constructor person');
     this.name = name;
   }
--- bin/student.dart
+++ bin/student.dart
@@ -1,7 +1,7 @@
 import 'person.dart';
 
 class Student extends Person {
-  Student() : super('student baru') {
+  Student() : super(name: 'student baru') {
     print('constructor student');
   }
 }
