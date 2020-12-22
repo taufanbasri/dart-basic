63b3338bbb78f509095005b2b782da398114080c
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2020-12-22 16:51:55 +0700

    Sort with 2 parameters

--- bin/main.dart
+++ bin/main.dart
@@ -2,16 +2,22 @@ void main(List<String> arguments) {
   List<Person> persons = [
     Person('Administrator', 32),
     Person('User', 54),
-    Person('Merchant', 32),
-    Person('Administrator', 32),
-    Person('User', 54),
-    Person('Merchant', 32),
-    Person('Administrator', 32),
-    Person('User', 54),
-    Person('Merchant', 32),
+    Person('Merchant', 22),
+    Person('Administrator', 30),
+    Person('User', 44),
+    Person('Merchant', 17),
+    Person('Administrator', 21),
+    Person('User', 43),
+    Person('Merchant', 37),
   ];
 
-  persons.sort((person1, person2) => person1.role.compareTo(person2.role));
+  persons.sort((person1, person2) {
+    if (person1.role.compareTo(person2.role) != 0) {
+      return person1.role.compareTo(person2.role);
+    } else {
+      return person1.age.compareTo(person2.age);
+    }
+  });
 
   persons.forEach((person) {
     print(person.role + ' - ' + person.age.toString());
