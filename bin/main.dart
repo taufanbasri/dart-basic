d2c37f1d7d2d926772e2f6d376d5d1793694177f
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2020-12-22 07:17:51 +0700

    Generic Type

--- bin/main.dart
+++ bin/main.dart
@@ -1,27 +1,13 @@
 import 'dart:io';
 
+import 'package:first_dart_console/generic_type.dart';
+
 void main(List<String> arguments) {
-  var versiRegular = ConstClass(number: 5);
-  var versiRegular2 = ConstClass(number: 5);
+  var box1 = SecureBox<String>('Halo', '123');
+  var box2 = SecureBox<int>(123, '123');
+  var box3 = SecureBox<DateTime>(DateTime.now(), '123');
 
-  print(identical(versiRegular,
-      versiRegular2)); // untuk mengecek 2 object yang berbeda, hasilnya akan false
-
-  var versiConst = const ConstClass(number: 5);
-  var versiConst2 = const ConstClass(number: 5);
-
-  print(identical(versiConst,
-      versiConst2)); // untuk mengecek 2 object yang berbeda, hasilnya akan true
-}
-
-class RegularClass {
-  final int number;
-
-  RegularClass({this.number});
-}
-
-class ConstClass {
-  final int number;
-
-  const ConstClass({this.number});
+  print(box1.getData('123').toString());
+  print(box2.getData('123').toString());
+  print(box3.getData('123').toString());
 }
